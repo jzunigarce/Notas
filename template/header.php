c1bd1f7ab1afb8df1e5d773f746a2711da19cc07
Jesús Antonio Zúñiga Arce <jzunigarce@gmail.com>
2017-11-21 01:50:02 -0700

    fix head

--- template/header.php
+++ template/header.php
@@ -1,9 +1,3 @@
-<?php session_start(); 
-  if(empty($_SESSION['user']) && $_SERVER['REQUEST_URI'] == '/notas.php')  
-    header("Location: /index.php");
-  else if(!empty($_SESSION['user']) && ($_SERVER['REQUEST_URI'] == '/index.php' || $_SERVER['REQUEST_URI'] == '/'))
-    header("Location: /notas.php");
-  ;?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -19,16 +13,6 @@
   </head>
   <body>
     <header class="container-fluid nav-bar pt-3 pb-3">
-      <?php if(!empty($_SESSION['user'])):
-      ?>
-        <h4 class="text-white text-right block">
-          <i class="fa fa-user" aria-hidden="true"></i>
-          <?php echo $_SESSION['user'];?> &nbsp;&nbsp;
-          <a class="text-white" href="controllers/userController.php?action=sign-out">
-            <i class="fa fa-sign-out" aria-hidden="true"></i>
-          </a>
-      </h4>
-      <?php endif; ?>
     	<h1 class="text-white text-center block">App notes <i class="fa fa-sticky-note" aria-hidden="true"></i>
 </h1>
     </header>
